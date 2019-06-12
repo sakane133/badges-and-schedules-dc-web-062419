2c96066ecdc0a08f9551704ac03ab40d86d0c1ed
Samantha Kane <s.kane133@gmail.com>
2019-06-12 14:19:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,4 @@
 # Write your code here.
 def badge_maker(person)
- num = 0 
- list = person.size
- while num < list
-    puts "Hello, my name is #{names}"
-    num += 1
-end 
+    puts "Hello, my name is #{person}"
 end
