b831b405a7dee271391e8c59784d53a2b39674d4
Samantha Kane <s.kane133@gmail.com>
2019-06-12 14:23:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def badge_maker(person)
-  person = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  person = []
   person.each do |name|
     return "Hello, my name is #{name}"
 end
